0390175c10a6a7ed5de10a6e287f355008ed2cb9
Miguel Rocha <miguelantoniooliveirarocha@gmail.com>
2018-10-17 23:28:30 +0100

    ball updated

--- js/Ball.js
+++ js/Ball.js
@@ -2,10 +2,11 @@ var material;
 
 class Ball extends Objeto {
 	constructor(pos_x, pos_y, pos_z){
-		this.pos_x = pos_x;
-		this.pos_y = pos_y;
-		this.pos_z = pos_z;
+		this.position.x = pos_x;
+        this.position.y = pos_y;
+        this.position.z = pos_z;
 		this.velocity = Math.randFloat(0.1, 5);
+		this.vector_mov = new THREE.Vector3(Math.randFloat(0,2), Math.randFloat(0,2), Math.randFloat(0,2));
 
 		return this;
 	}
