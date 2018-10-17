3e829142b35f9b71b7db578b3c37988f59ebd317
Miguel Rocha <miguelantoniooliveirarocha@gmail.com>
2018-10-17 23:53:35 +0100

    edited ball

--- js/Ball.js
+++ js/Ball.js
@@ -2,9 +2,9 @@ var material;
 
 class Ball extends Object {
 	constructor(pos_x, pos_y, pos_z){
-		this.position.x = pos_x;
-		this.position.y = pos_y;
-		this.position.z = pos_z;
+		this.createMesh(new THREE.SphereGeometry(RADIUS, 32, 32), pos_x, pos_y, pos_z);
+		this.velocity = Math.randFloat(0.1, 5);
+		this.vector_mov = new THREE.Vector3(Math.randFloat(0,2), Math.randFloat(0,2), Math.randFloat(0,2));
 
 		return this;
 	}
