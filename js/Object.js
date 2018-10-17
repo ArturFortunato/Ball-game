b1e0bc6fd420766650214be0a42cbdd96ad589a5
Filipe Reynaud <reynaudfilipe@gmail.com>
2018-10-17 23:33:29 +0100

    material

--- js/Object.js
+++ js/Object.js
@@ -4,15 +4,15 @@ class Object extends THREE.Object3D {
         super();
     }
 
-    createMesh(geometry, x, y, z) {
+    createMesh(geometry, material, x, y, z) {
         var mesh = new THREE.Mesh(geometry, material);
         mesh.position.set(x, y, z);
 
         return mesh;
     }
 
-    addElement(x, y, z, geometry) {
-		this.add(this.createMesh(geometry, x, y, z));
+    addElement(x, y, z, geometry, material) {
+		this.add(this.createMesh(geometry, material, x, y, z));
     }
 
 }
--- js/Table.js
+++ js/Table.js
@@ -2,11 +2,11 @@ var material;
 
 class Table extends Object {
     constructor() {
-        table.addElement(0, -0.25, 0, new THREE.BoxGeometry(20, 0.5, 10));
-        table.addElement(8, -2, -3, new THREE.CylinderGeometry(0.4, 0.2, 3, 10));
-        table.addElement(-8, -2, -3, new THREE.CylinderGeometry(0.4, 0.2, 3, 10));
-        table.addElement(8, -2, 3, new THREE.CylinderGeometry(0.4, 0.2, 3, 10));
-        table.addElement(-8, -2, 3, new THREE.CylinderGeometry(0.4, 0.2, 3, 10));
+        table.addElement(0, -0.25, 0, new THREE.BoxGeometry(20, 0.5, 10), TABLE_MATERIAL);
+        table.addElement(8, -2, -3, new THREE.CylinderGeometry(0.4, 0.2, 3, 10), TABLE_MATERIAL);
+        table.addElement(-8, -2, -3, new THREE.CylinderGeometry(0.4, 0.2, 3, 10), TABLE_MATERIAL);
+        table.addElement(8, -2, 3, new THREE.CylinderGeometry(0.4, 0.2, 3, 10), TABLE_MATERIAL);
+        table.addElement(-8, -2, 3, new THREE.CylinderGeometry(0.4, 0.2, 3, 10), TABLE_MATERIAL);
 
         return this;
     }
