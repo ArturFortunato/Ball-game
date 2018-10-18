e04cd8e1f0e7fbe8073f34436ec0eab6e49f6e61
Miguel Rocha <miguelantoniooliveirarocha@gmail.com>
2018-10-18 19:33:54 +0100

    bola desparece com colisao mas outra está bem

--- js/game.js
+++ js/game.js
@@ -35,7 +35,33 @@ class Game {
         for (var j = 0; j < NUM_BALLS; j++) {
             if(i != j)
                 if(this.ball_list[i].colides(this.ball_list[j])) {
-                    //Formula do enunciado
+                	var b1_v = this.ball_list[i].velocity;
+                	var b2_v = this.ball_list[j].velocity;
+
+                	var b1_mass = this.ball_list[i].mass;
+                	var b2_mass = this.ball_list[j].mass;
+
+                	var b1_c = new THREE.Vector3(this.ball_list[i].x, 0 , this.ball_list[i].z);
+                	console.log(b1_c);
+                	var b2_c = new THREE.Vector3(this.ball_list[j].x, 0, this.ball_list[j].z);
+
+                	var first_parcel = (2*b2_mass) / (b1_mass+b2_mass);
+                	console.log(first_parcel);
+
+                	var vector_mult = (b1_v.add(b2_v.negate())).dot(b1_c.add(b2_c.negate()));
+                	console.log(vector_mult);
+                	var norma_aux = Math.sqrt(Math.pow(b1_c.x - b2_c.x, 2) + Math.pow(b1_c.y - b2_c.y, 2) + Math.pow(b1_c.z - b2_c.z, 2));
+                	var norma = Math.pow(norma_aux, 2);
+                	console.log(norma);
+                	var second_parcel_aux = vector_mult/norma;
+                	var second_parcel = (b1_c.add(b2_c.negate())).multiplyScalar(second_parcel_aux);
+                	var total = second_parcel.multiplyScalar(first_parcel);
+
+                	var velocity = b1_v.add(total.negate());
+
+                	this.ball_list[i].velocity = velocity;
+                	this.ball_list[j].velocity = -velocity;
+                    
                     return true;
                 }
         }
