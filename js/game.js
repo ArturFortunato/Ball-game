c1af78848590a1ada83ced53a6b039bd9d79c636
Artur Fortunato <artur.fortunato1@gmail.com>
2018-10-19 13:33:01 +0100

    Colisoes completas

--- js/game.js
+++ js/game.js
@@ -16,10 +16,10 @@ class Game {
             }
             this.addBall(new Ball(x, RADIUS, z, 1));
         }
+        setInterval(this.increaseVelocities, 6000, this);
     }
 
     addBall(ball) {
-
         this.ball_list.push(ball);
         scene.add(ball);
     }
@@ -31,12 +31,19 @@ class Game {
         return false;
     }
 
+    increaseVelocities(game) {
+        for(var i = 0; i < NUM_BALLS; i++)
+            if(Math.abs(game.ball_list[i].velocity.x) < MAX_VELOCITY && Math.abs(game.ball_list[i].velocity.y) < MAX_VELOCITY && Math.abs(game.ball_list[i].velocity.z) < MAX_VELOCITY) {
+                game.ball_list[i].increaseVelocity();
+            }
+    }
+
     getVelocityAfterColision(ball1, ball2) {
         var toReturn = ball1.velocity.clone();
         var vel1 = ball1.velocity.clone();
         var vel2_n = ball2.velocity.clone().negate();
-        var distance_centers_sqr = Math.pow(ball1.x - ball2.x, 2) + Math.pow(ball1.y - ball2.y, 2) + Math.pow(ball1.z - ball2.z, 2);
-        var vector_between_centers = new THREE.Vector3(ball1.x - ball2.x, ball1.y - ball2.y, ball1.z - ball2.z);
+        var distance_centers_sqr = Math.pow(ball1.mesh.position.x - ball2.mesh.position.x, 2) + Math.pow(ball1.mesh.position.y - ball2.mesh.position.y, 2) + Math.pow(ball1.mesh.position.z - ball2.mesh.position.z, 2);
+        var vector_between_centers = new THREE.Vector3(ball1.mesh.position.x - ball2.mesh.position.x, ball1.mesh.position.y - ball2.mesh.position.y, ball1.mesh.position.z - ball2.mesh.position.z);
 
         var scalar1 = (2 * ball2.mass) / (ball1.mass + ball2.mass);
         var scalar2 = vel1.add(vel2_n).dot(vector_between_centers);
@@ -49,31 +56,43 @@ class Game {
         for (var j = 0; j < NUM_BALLS; j++) {
             if(i != j)
                 if(this.ball_list[i].colides(this.ball_list[j])) {
-                	var position1 = this.getVelocityAfterColision(this.ball_list[i], this.ball_list[j]);
+                    var separate = -(2 * RADIUS - this.ball_list[i].getDistance(this.ball_list[j].mesh.position.x, this.ball_list[j].mesh.position.y, this.ball_list[j].mesh.position.z)) / 2;
+                    this.ball_list[i].mesh.position.add(this.ball_list[i].velocity.clone().normalize().multiplyScalar(separate));
+                    this.ball_list[j].mesh.position.add(this.ball_list[j].velocity.clone().normalize().multiplyScalar(separate));
+                                      
+                    var position1 = this.getVelocityAfterColision(this.ball_list[i], this.ball_list[j]);
                     this.ball_list[j].velocity.copy(this.getVelocityAfterColision(this.ball_list[j], this.ball_list[i]));
                     this.ball_list[i].velocity.copy(position1);	                    
-                    return true;
                 }
         }
-        return false;
     }
     
     colidesWithWall(ball){
-        if (Math.abs(ball.x) >= (10 - RADIUS))
+        if (Math.abs(ball.mesh.position.x) >= (10 - RADIUS)) {
+            if (ball.mesh.position.x > 0)
+                ball.mesh.position.x = 10 - RADIUS;
+            else
+                ball.mesh.position.x = RADIUS - 10;
             ball.changeVelocity(-ball.velocity.x, 0, ball.velocity.z);
-        else if (Math.abs(ball.z) >= (5 - RADIUS))
+        }
+        else if (Math.abs(ball.mesh.position.z) >= (5 - RADIUS)) {
+            if (ball.mesh.position.z > 0)
+                ball.mesh.position.z = 5 - RADIUS;
+            else
+                ball.mesh.position.z = RADIUS - 5;
             ball.changeVelocity(ball.velocity.x, 0, -ball.velocity.z);
+        }
     }
 
     refresh() {
         var time = clock.getDelta();
 
-        for(var i = 0; i < NUM_BALLS; i++) {
-            this.colidesWithBalls(i)
-
-            this.colidesWithWall(this.ball_list[i])
-            
+        for(var i = 0; i < NUM_BALLS; i++) {            
             this.ball_list[i].moveBall(time);
+
+            this.colidesWithBalls(i);
+
+            this.colidesWithWall(this.ball_list[i]);
         }
     }
 }
